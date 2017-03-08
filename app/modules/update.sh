2bf56e7ae3b9bb5cfc8c1658f611112a9e1e5f06
dking3876 <dking3876@msn.com>
2017-03-08 13:22:22 -0500

    add check for existing tag before adding new deployment to deployment file, fix adding new deployment to deployment file

--- app/modules/update.sh
+++ app/modules/update.sh
@@ -4,33 +4,32 @@ echo "##        Hermes Automated Deployment"
 echo "#########################################################"
 echo "Updating Hermes."
 #download hermes with wget to tmp directory
-echo "hermes auto update is not currently available.  Please see docs at https://github.com/hermes-ci"
-exit;
-wget -O /tmp/hermesupdate/latest.tar.gz https://github.com/hermes-ci/hermes/archive/v1.0.6-beta.tar.gz
-tar -xvzf /tmp/hermesupdate/latest.tar.gz
+echo "hermes auto update is not currently complete.  Please see docs at https://github.com/hermes-ci before answering 'yes'"
+#wget -O /tmp/hermesupdate/latest.tar.gz https://github.com/hermes-ci/hermes/archive/v1.0.6-beta.tar.gz
+#tar -xvzf /tmp/hermesupdate/latest.tar.gz
 per=0
 while true; do
-    read -p "Would you like to update Hermes now? [y/n] " answer
+    read -p "Would you like to update Hermes now? (have you copied new files) [y/n] " answer
     case $answer in
         [Yy]* ) 
             echo -ne "|"
             while [ $per -lt 7 ]; do
-                echo -ne "##\r"
+                echo -ne "##"
                 sleep 1
                 ((per++))
             done
             echo -ne '##|(100%)'
             sleep 1
 
-            cp -R /tmp/hermesupdate/app/* "$ROOT"/app
+            #cp -R /tmp/hermesupdate/app/* "$ROOT"/app
             
-            chmod +x "$ROOT"/app/*.sh
+            chmod -R +x "$ROOT"/app/*.sh
             chmod +x "$ROOT"/app/hermes-ci
             echo -ne '\n'
             echo "Hermes update completed";
             #Create symlink to heremes file in the bin path to use everywhere
             ln -sf "$ROOT"/app/hermes-ci /bin/hermes-ci
-            rm -R /tmp/hermesupdate
+            #rm -R /tmp/hermesupdate
             hermes-ci sshconfig
             exit;;
         [Nn]* ) exit 150;;
--- app/setup.sh
+++ app/setup.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 filename=$DEPLOYMENTCONFIG/$ACCOUNT-$REPO;
-if [ -f $filename ]
+if [ -f $filename.deployment.json ]
 then
     echo "file exists need to check if tag exists and if not add it to the deploy array"
     #check for tag
 
     #tag does not exist in deploy array setup new deployment
-    newdeploy=<<EOL
-{
-    "tag": "$TAG",
-    "branch": "$BRANCH",
-    "beforeinstall": "$BEFOREINSTALL",
-    "afterinstall": "$AFTERINSTALL",
-    "source": "$SOURCE",
-    "target": "$TARGET"
-}
-EOL
-    #add new deployment to array and save new deployment file
-    content=$(cat $filename | jq .deploy |= .+ [$newdeploy])
-    echo $content > $filename.deployment.json
-    if [ $? = 0 ]
+    exists=$(cat $filename.deployment.json | jq '.deploy[] | select(.tag=="'$TAG'")')
+    if [ "$exists" != "" ]
     then
-        echo "Succesfully saved deployment for $filename . Run hermes-ci deploy --config $filename --tag $tag to run this deployment"
+        #add new deployment to array and save new deployment file
+    content=$(cat $filename.deployment.json | jq '.deploy |= .+ [{
+        "tag": "'$TAG'",
+        "branch": "'$BRANCH'",
+        "beforeinstall": "'$BEFOREINSTALL'",
+        "afterinstall": "'$AFTERINSTALL'",
+        "source": "'$SOURCE'",
+        "target": "'$TARGET'"
+    }]')
+        echo $content > $filename.deployment.json
+        if [ $? = 0 ]
+        then
+            echo "Succesfully saved deployment for $filename . Run hermes-ci deploy --config $filename --tag $TAG to run this deployment"
+        else
+            echo "There was an error saving your deployment"
+        fi
     else
-        echo "There was an error saving your deployment"
+        echo "Deployement already exists with $TAG tag.  To modify a deployment it is best to add a hermes.json file to your project.  You can alternatively modify the deployment file manually"
     fi
 else
     if [ "$NAME" = "" ]
