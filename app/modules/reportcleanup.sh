a913a348f733e16559af0fda0c6432c8a499a4be
dking3876 <dking3876@msn.com>
2017-03-08 23:31:13 -0500

    correct incosistancies and fix operator for report size

--- app/modules/reportcleanup.sh
+++ app/modules/reportcleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 REPORT="$LOGS/$ACCOUNT"_"$REPO"
 REPORTSIZE=$(stat -c%s "$REPORT")
-if [ $REPORTSIZE -gt 2500000 ] || [[ $(date +%u) -eq 7 ]]
+if [[ $REPORTSIZE -gt 2500000 ]] || [[ $(date +%u) -eq 7 ]]
 then
 	mv "$REPORT" "$REPORT-$(date +"%Y%m%d")"
 fi
